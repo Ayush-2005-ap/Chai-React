7c4fd5560081cb6b709b367e109660859a2a3504
Ayush Pandey <apayush.20@gmail.com>
2025-07-08 16:24:25 +0530

    final Commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username:"Hitesh",
+    age:21
+  }
+
   return (
     <>
-      <h1 className='bg-green-400 text-black '>Tailwind Test</h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card  username="chaiaurcode"  />
+      <Card  username="Ayush"/>
+
     </>
   )
 }
